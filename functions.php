cf398e34904c6557e9f7ec2b6a43a1b3c20e81aa
23232x <23232x@gmail.com>
2022-11-18 16:55:12 -0300

    add novos recursos

--- functions.php
+++ functions.php
@@ -6,11 +6,11 @@
 // ******* Function to registry JS into wordpress pages *********
 function polo_scripts(){  
   wp_register_script('teste', get_template_directory_uri() . '/assets/js/teste.js', [], false, true);
-  wp_register_script('swipercdn', get_template_directory_uri() . 'https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js', [], false, true);
-  wp_register_script('swiper', get_template_directory_uri() . '/assets/js/swiper.js', ['swipercdn'], false, true);
+  // wp_register_script('swipercdn', get_template_directory_uri() . 'https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js', [], false, true);
+  wp_register_script('swiper', get_template_directory_uri() . '/assets/js/swiper.js', [], false, true);
   
   
-  wp_enqueue_script('teste', 'swiper');
+  wp_enqueue_script('teste', 'swipercdn', 'swiper');
 }
 // this functions "polo_scripts", run when the
 add_action( 'wp_enqueue_scripts', 'polo_scripts' );
