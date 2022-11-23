0f87ce410db83d1840f60ab5349a5d51d8291115
23232x <23232x@gmail.com>
2022-11-23 14:22:09 -0300

    adde style instituicoes parceiras

--- page-home.php
+++ page-home.php
@@ -14,17 +14,46 @@
 
         <!-- this section contains the universety partner of polo UAB Sapiranga -->
 
-
-
-        <section class="partners">
-
-            <ul>
-                <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/partners/ufrgs.png" alt=""></a></li>
-            </ul>
+<main>
 
 
 
-        </section>
+
+
+
+
+<!-- this section contains the universety partner of polo UAB Sapiranga -->
+<section class="partners container">
+<h4>Instituções Parceiras</h4>
+
+
+
+
+
+<ul>
+    <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/partners/furg.png" alt=""></a></li>
+    <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/partners/ifsul.png" alt=""></a></li>
+    <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/partners/pms.png" alt=""></a></li>
+    <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/partners/ufpel.png" alt=""></a></li>
+    <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/partners/ufrgs.png" alt=""></a></li>
+
+</ul>
+
+
+
+</section>
+
+
+
+</main>
+
+
+
+
+
+
+
+
 
 
         <!-- fim do loope principal -->
