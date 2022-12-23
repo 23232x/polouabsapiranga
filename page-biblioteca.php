ea58c8714c3d81a4dd313b8d196e8ced27e5a4db
23232x <23232x@gmail.com>
2022-12-23 12:58:47 -0300

    added itens

--- page-biblioteca.php
+++ page-biblioteca.php
@@ -9,7 +9,6 @@
 
 
         <section class="section container">
-
             <!-- all subpages contains this section socials icon on the top page -->
             <div class="social-medias-top-page">
 
@@ -70,7 +69,6 @@
                 <ul class="sub-tittle-section">
                     <li>
                         <span><?php the_field('subtitulo_pagina_biblioteca'); ?></span>
-
                     </li>
                 </ul>
             </div>
@@ -80,6 +78,36 @@
             </div>
 
 
+        </section>
+
+
+        <!-- page contend -->
+        <section class="main-page bg">
+
+
+
+            <div class="container wraper-contend-section">
+
+                <div class="library-titles">
+                    <h4>Bibliotecas Universitárias</h4>
+                    <p> Links de acesso aos sites das Bibliotecas das Universidades participantes da UAB, bem como os sites de consulta ao acervo.</p>
+                </div>
+
+                <div class="contend-section container">
+                    <div class="link-item">
+                        <!-- add class 'on' with js - must be like this <div class="faq-title on"> -->
+                        <div class="link-title on">
+                            <h4 id="link01">FURG</h4>
+                            <img class="cross" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/faq/plus.svg" alt="">
+                        </div>
+                        <ul style="display: block;">
+                            <li><a href="https://argo.furg.br/" target="_blank">Consulta ao acervo FURG</a></li>
+                            <li><a href="http://www.biblioteca.furg.br/">Biblioteca FURG</a></li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
         </section>
 
 
--- page-faq.php
+++ page-faq.php
@@ -14,48 +14,47 @@
         <div class="section container">
             <!-- all subpages contains this section socials icon on the top page -->
             <div class="social-medias-top-page">
-
                 <!-- Social medias -->
                 <ul>
                     <!-- facebook -->
                     <li>
                         <a href="#">
-                            <img src="assets/img/icons/social-medias-top-page/face.svg" alt="Facebook Polo Universitário UAB - Sapiranga">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/social-medias-top-page/face.svg" alt="Facebook Polo Universitário UAB - Sapiranga">
                         </a>
                     </li>
 
                     <!-- instagram -->
                     <li>
                         <a href="#">
-                            <img src="assets/img/icons/social-medias-top-page/insta.svg" alt="Instagram Polo Universitário UAB - Sapiranga">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/social-medias-top-page/insta.svg" alt="Instagram Polo Universitário UAB - Sapiranga">
                         </a>
                     </li>
 
                     <!-- youtube -->
                     <li>
                         <a href="#">
-                            <img src="assets/img/icons/social-medias-top-page/youtube.svg" alt="Youtube Polo Universitário UAB - Sapiranga">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/social-medias-top-page/youtube.svg" alt="Youtube Polo Universitário UAB - Sapiranga">
                         </a>
                     </li>
 
                     <!-- whatsapp -->
                     <li>
                         <a href="#">
-                            <img src="assets/img/icons/social-medias-top-page/whats.svg" alt="whatsapp Polo Universitário UAB - Sapiranga">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/social-medias-top-page/whats.svg" alt="whatsapp Polo Universitário UAB - Sapiranga">
                         </a>
                     </li>
 
                     <!-- email -->
                     <li>
                         <a href="#">
-                            <img src="assets/img/icons/social-medias-top-page/email.svg" alt="email Polo Universitário UAB - Sapiranga">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/social-medias-top-page/email.svg" alt="email Polo Universitário UAB - Sapiranga">
                         </a>
                     </li>
 
                     <!-- print -->
                     <li>
                         <a href="#">
-                            <img src="assets/img/icons/social-medias-top-page/print.svg" alt="Polo Universitário UAB - Sapiranga">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/social-medias-top-page/print.svg" alt="Polo Universitário UAB - Sapiranga">
                         </a>
                     </li>
                 </ul>
@@ -67,8 +66,7 @@
                 <ul class="sub-tittle-section">
                     <li>
                         <span>
-                            Se alguma de suas perguntas não for respondida abaixo, envie nos sua pergunta través deste
-                            email
+                            Selecionamos uma lista de perguntas frequêntes para te ajudar
                             <a href="mailto:adicicionaremail?subject=Pergunta" class="email-link">polouab@polouab.com.br</a>
                         </span>
                     </li>
@@ -79,30 +77,182 @@
         <!-- page contend -->
         <section class="main-page bg">
             <div class="contend-section container">
+
+
+                <!--*********** 1° FAQ ************** -->
                 <div class="faq-item">
                     <!-- add class 'on' with js - must be like this <div class="faq-title on"> -->
-                    <div class="faq-title">
+                    <div class="faq-title on">
                         <h4 id="faq01">O que é a UAB?</h4>
-                        <img class="cross" src="assets/img/icons/faq/plus.svg" alt="">
+                        <img class="cross" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/faq/plus.svg" alt="">
                     </div>
-                    <p class="faq-answer" style="display: none;">É um programa federal que visa ampliar e interiorizar a
+                    <p class="faq-answer" style="display: block;">É um programa federal que visa ampliar e interiorizar a
                         oferta de cursos e
                         programas de educação superior, por meio da educação a distância.
                     </p>
                 </div>
 
 
-                <div class="faq-item">
 
+                <!--*********** 2° FAQ ************** -->
+                <div class="faq-item">
                     <!-- add class 'on' with js - must be like this <div class="faq-title on"> -->
-                    <div class="faq-title">
-                        <h4 id="faq01">O que é a UAB?</h4>
-                        <img class="cross" src="assets/img/icons/faq/plus.svg" alt="">
+                    <div class="faq-title on">
+                        <h4 id="faq02">O que é um Polo UAB?</h4>
+                        <img class="cross" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/faq/plus.svg" alt="">
                     </div>
-                    <p class="faq-answer" style="display: none;">É um programa federal que visa ampliar e interiorizar a
-                        oferta de cursos e programas de educação superior, por meio da educação a distância.
+                    <p class="faq-answer" style="display: block;">
+                        É um local para o desenvolvimento de atividades pedagógicas presenciais, em que os alunos entram em contato com tutores e professores.
                     </p>
                 </div>
+
+                <!--*********** 3° FAQ ************** -->
+                <div class="faq-item">
+                    <!-- add class 'on' with js - must be like this <div class="faq-title on"> -->
+                    <div class="faq-title on">
+                        <h4 id="faq03">Quem é o mantenedor do Polo UAB?</h4>
+                        <img class="cross" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/faq/plus.svg" alt="">
+                    </div>
+                    <p class="faq-answer" style="display: block;">
+                        A Prefeitura Municipal.
+                    </p>
+                </div>
+
+                <!--*********** 4° FAQ ************** -->
+                <div class="faq-item">
+                    <!-- add class 'on' with js - must be like this <div class="faq-title on"> -->
+                    <div class="faq-title on">
+                        <h4 id="faq04">Qual a infraestrutura do Polo?</h4>
+                        <img class="cross" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/faq/plus.svg" alt="">
+                    </div>
+                    <p class="faq-answer" style="display: block;">
+                        O Polo oferece, sala de coordenação, secretaria, sala de tutoria, salas de aula, laboratórios de informática e biblioteca.
+                    </p>
+                </div>
+
+                <!--*********** 5° FAQ ************** -->
+                <div class="faq-item">
+                    <!-- add class 'on' with js - must be like this <div class="faq-title on"> -->
+                    <div class="faq-title on">
+                        <h4 id="faq05">Qual a prioridade dos cursos nos Polos UAB?</h4>
+                        <img class="cross" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/faq/plus.svg" alt="">
+                    </div>
+                    <p class="faq-answer" style="display: block;">
+                        A prioridade é a formação de professores em efetivo exercício na educação básica pública, mas que ainda não possuem graduação, além de proporcionar formação continuada àqueles já graduados.
+                    </p>
+                </div>
+
+
+                <!--*********** 6° FAQ **************-->
+                <div class="faq-item">
+                    <!-- add class 'on' with js - must be like this <div class="faq-title on"> -->
+                    <div class="faq-title on">
+                        <h4 id="faq06">Quais os objetivos do programa UAB?</h4>
+                        <img class="cross" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/faq/plus.svg" alt="">
+                    </div>
+                    <p class="faq-answer" style="display: block;">
+                        Os principais objetivos deste programa são:
+                    </p>
+                    <ul class="list-faq">
+                        <li>
+                            <p class="faq-answer">oportunizar a oferta de cursos para dirigentes, gestores e outros profissionais da educação básica da rede pública;</p>
+                        </li>
+                        <li>
+                            <p class="faq-answer">reduzir as desigualdades na oferta de ensino superior e desenvolver um amplo sistema nacional de educação superior a distância.</p>
+                        </li>
+                        <li>
+                            <p class="faq-answer">formar professores e outros profissionais de educação nas áreas da diversidade;</p>
+                        </li>
+                        <li>
+                            <p class="faq-answer">disseminação e desenvolvimento de metodologias educacionais de inserção dos temas de áreas como educação de jovens e adultos, educação ambiental, educação patrimonial, educação para os direitos humanos, educação das relações étnico-raciais, de gênero e orientação sexual e temas da atualidade no cotidiano das práticas das redes de ensino pública e privada de educação básica no Brasil.</p>
+                        </li>
+                    </ul>
+                </div>
+
+                <!--*********** 7° FAQ ************** -->
+                <div class="faq-item">
+                    <!-- add class 'on' with js - must be like this <div class="faq-title on"> -->
+                    <div class="faq-title on">
+                        <h4 id="faq07">Os cursos são gratuitos?</h4>
+                        <img class="cross" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/faq/plus.svg" alt="">
+                    </div>
+                    <p class="faq-answer" style="display: block;">
+                        Os cursos de graduação sim , o candidato paga somente a inscrição para o vestibular. Já, os cursos de pós graduação, fica a critério da Universidade a cobrança de mensalidade ou não mas, até o momento, todos os cursos oferecidos são gratuitos.
+                    </p>
+                </div>
+
+                <!--*********** 8° FAQ ************** -->
+                <div class="faq-item">
+                    <!-- add class 'on' with js - must be like this <div class="faq-title on"> -->
+                    <div class="faq-title on">
+                        <h4 id="faq08">Qual o tempo de duração?</h4>
+                        <img class="cross" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/faq/plus.svg" alt="">
+                    </div>
+                    <p class="faq-answer" style="display: block;">
+                        O mesmo tempo da duração dos cursos de graduação, são de 8 a 10 semestres dependendo do curso, já os de pós, são de 18 a 24 meses.
+                    </p>
+                </div>
+
+                <!--*********** 9° FAQ ************** -->
+                <div class="faq-item">
+                    <!-- add class 'on' with js - must be like this <div class="faq-title on"> -->
+                    <div class="faq-title on">
+                        <h4 id="faq09">Qual a forma de ingresso?</h4>
+                        <img class="cross" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/faq/plus.svg" alt="">
+                    </div>
+                    <p class="faq-answer" style="display: block;">
+                        A universidade que oferece o curso pode optar por vestibular ou pela nota do ENEM.
+                    </p>
+                </div>
+
+                <!--*********** 10° FAQ ************** -->
+                <div class="faq-item">
+                    <!-- add class 'on' with js - must be like this <div class="faq-title on"> -->
+                    <div class="faq-title on">
+                        <h4 id="faq10">Qual período do ano os editais são publicados?</h4>
+                        <img class="cross" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/faq/plus.svg" alt="">
+                    </div>
+                    <p class="faq-answer" style="display: block;">
+                        Não existe uma data específica, pode ser no início, no meio ou no final do ano. Os cursos têm início no primeiro ou segundo semestre, assim, o edital é divulgado em torno de 3 a 4 meses antes para que haja tempo de divulgação e realização do processo seletivo e para as matrículas.
+                    </p>
+                </div>
+
+                <!--*********** 11° FAQ ************** -->
+                <div class="faq-item">
+                    <!-- add class 'on' with js - must be like this <div class="faq-title on"> -->
+                    <div class="faq-title on">
+                        <h4 id="faq11">Os cursos são oferecidos por universidades públicas e privadas?</h4>
+                        <img class="cross" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/faq/plus.svg" alt="">
+                    </div>
+                    <p class="faq-answer" style="display: block;">
+                        Pelo sistema UAB, somente por universidades públicas.
+                    </p>
+                </div>
+
+                <!--*********** 12° FAQ ************** -->
+                <div class="faq-item">
+                    <!-- add class 'on' with js - must be like this <div class="faq-title on"> -->
+                    <div class="faq-title on">
+                        <h4 id="faq12">Posso estar matriculado em dois cursos simultaneamente?</h4>
+                        <img class="cross" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/faq/plus.svg" alt="">
+                    </div>
+                    <p class="faq-answer" style="display: block;">
+                        Se forem duas especializações ou uma graduação e uma especialização sim, mas graduação não pode. Precisa concluir uma para se matricular em outra.
+                    </p>
+                </div>
+
+                <!--*********** 13° FAQ ************** -->
+                <div class="faq-item">
+                    <!-- add class 'on' with js - must be like this <div class="faq-title on"> -->
+                    <div class="faq-title on">
+                        <h4 id="faq13">Como é estudar na UAB?</h4>
+                        <img class="cross" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/faq/plus.svg" alt="">
+                    </div>
+                    <p class="faq-answer" style="display: block;">
+                        Estudar na Universidade Aberta (UAB) não é fácil: exige muita motivação, organização e disciplina. Mas tem inúmeras vantagens: sobretudo a possibilidade de gerir o tempo, conciliar os estudos com as obrigações familiares e profissionais.
+                    </p>
+                </div>
+
             </div>
         </section>
 
